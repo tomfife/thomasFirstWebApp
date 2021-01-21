4f9506c0b6089eb708aa68341821d4f18fd82013
tomfife <71194993+tomfife@users.noreply.github.com>
2021-01-20 17:21:41 -0700

    First commit

---
+++ wwwroot/js/fanmail.js
@@ -0,0 +1,9 @@
+﻿var submit = document.getElementById("submitButton");
+
+submit.addEventListener("click", function () {
+    alert("From = " + document.getElementById("from").value + ", " +
+        "Email = " + document.getElementById("email").value + ", " +
+        "Subject = " + document.getElementById("subject").value + ", " +
+        "Message = " + document.getElementById("message").value);
+});
+alert
